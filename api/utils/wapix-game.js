c2e5daa758d4ae05aed63406f043082913d2ca70
GustavoRuedaEnriquez <gusgus2208@gmail.com>
2020-11-21 14:55:06 -0600

    Class started

---
+++ api/utils/wapix-game.js
@@ -0,0 +1,49 @@
+class WapixGame {
+    constructor() {
+        this.wapix = '';
+        this.date = Date();
+        this.players = [];
+        this.results = [];
+    }
+
+    getWapixId() {
+        return this.wapix;
+    }
+
+    getWapixDate() {
+        return this.date;
+    }
+
+    getPlayers() {
+        return this.players;
+    }
+
+    getResults() {
+        return this.results;
+    }
+
+    setWapixId(wapixId) {
+        this.wapix = wapixId;
+    }
+
+    setWapixDate(date) {
+        this.date = date;
+    }
+
+    fillbasicInfo(wapixInfo) {
+        this.setWapixId(wapixInfo.wapixId);
+    }
+
+    newPlayer(username, hostId) {
+        let player = {
+            id = `${Date()}${Math.random() * 1000}`,
+            username = username,
+            hostId = hostId
+        }
+        return player;
+    }
+
+    addPlayer(player) {
+        this.players.push(player);
+    }
+}
